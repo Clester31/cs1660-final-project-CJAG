7c2372329f423d45412be7ee6c1ea928625d34c2
Julie Lawler <143622300+jal355@users.noreply.github.com>
2025-04-16 18:55:03 -0400

    Update get and post in main

--- cloud_run/main.py
+++ cloud_run/main.py
@@ -1,13 +1,21 @@
 # Feel free to edit whatever you need. Just wanted to get it set up so that I can integrate this with the AJAX later on - Christian 
-
+# Names collection in firestore "signed-in" - Julie
 from fastapi import FastAPI, Form, Request, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from google.cloud import firestore
+import datetime
+from typing import Annotated
 
 app = FastAPI()
 
 # make sure to also mount any static files
+app.mount("/static", StaticFiles(directory="/app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+
+# init firestore client
+db = firestore.Client()
+student_collection = db.collection("signed-in")
 
 # for testing purposes
 students_signed_in = [
@@ -18,7 +26,24 @@ students_signed_in = [
 
 @app.get("/")
 async def read_root(request: Request):
+    signed_in = student_collection.stream()
+    # @note: we are storing the votes in `vote_data` list because the firestore stream closes after certain period of time
+    students_signed_in = []
+    
+    for s in signed_in:
+        students_signed_in.append(s.to_dict())
+    
     return templates.TemplateResponse("index.html", {
         "request": request,
         "students_signed_in": students_signed_in
     })
+    
+@app.post("/")
+async def create_vote(name: Annotated[str, Form()]):
+
+    student_collection.add({
+        "name": name,
+        "time": datetime.datetime.utcnow().isoformat()
+    })
+    
+    return {"message": f"Sign in for {name} received!"}
