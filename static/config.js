cb82fa8c661de0be5a056dc8673a1d4880a465ae
Akshitha Viswanadhula <akv18@pitt.edu>
2025-04-16 17:39:10 -0400

    added identitiy platform clident sdk

---
+++ static/config.js
@@ -0,0 +1,10 @@
+{/* <script src="https://www.gstatic.com/firebasejs/8.0/firebase.js"></script>
+<script>
+  var config = {
+    apiKey: "AIzaSyAzQh6QzrPzDHqM6BdV31egu-jCRy-qmDA",
+    authDomain: "attendence-tracker-457021.firebaseapp.com",
+  };
+  firebase.initializeApp(config);
+</script> */}
+
+// the stuff above is to initialize the Identity Platform Client SDK.
