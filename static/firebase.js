e90fd1700d05afeab18352715bbcb02bde639527
Julie Lawler <143622300+jal355@users.noreply.github.com>
2025-04-20 18:40:03 -0400

    Update firebase.js

---
+++ static/firebase.js
@@ -0,0 +1,140 @@
+firebase.initializeApp(config);
+
+// Watch for state change from sign in
+function initApp() {
+  firebase.auth().onAuthStateChanged(user => {
+    const signInButton = document.getElementById('signInButton');
+    if (user) {
+      // User is signed in.
+      signInButton.innerText = 'Sign Out';
+      document.getElementById('form').style.display = '';
+    } else {
+      // No user is signed in.
+      signInButton.innerText = 'Sign In with Google';
+      document.getElementById('form').style.display = 'none';
+    }
+  });
+}
+
+// check if authentication is disabled via query parameter
+function authDisabled() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const hostname = window.location.hostname;
+  // Auth is disabled only if running on localhost and `auth=false` is passed
+  return urlParams.get('auth') === 'false' && hostname === 'localhost';
+}
+
+// create ID token
+async function createIdToken() {
+  if (authDisabled()) {
+    console.warn('Auth is disabled. Returning dummy ID token.');
+    return new Promise((resolve) => {
+        resolve('dummyToken');  // return a dummy ID token
+    })
+  } else {
+    return await firebase.auth().currentUser.getIdToken();
+  }
+}
+
+window.onload = function () {
+  if (authDisabled()) {
+    console.warn('Running with auth disabled.');
+    document.getElementById('signInButton').innerText = '(Auth Disabled)';
+    document.getElementById('form').style.display = '';
+  } else {
+    console.log('Running with auth enabled.');
+    initApp();
+  }
+};
+
+function signIn() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/userinfo.email');
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(result => {
+      // Returns the signed in user along with the provider's credential
+      console.log(`${result.user.displayName} logged in.`);
+      window.alert(`Welcome ${result.user.displayName}!`);
+    })
+    .catch(err => {
+      console.log(`Error during sign in: ${err.message}`);
+      window.alert(`Sign in failed. Retry or check your browser logs.`);
+    });
+}
+
+function signOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(result => {})
+    .catch(err => {
+      console.log(`Error during sign out: ${err.message}`);
+      window.alert(`Sign out failed. Retry or check your browser logs.`);
+    });
+}
+
+// Toggle Sign in/out button
+function toggle() {
+  if (authDisabled()) {
+    window.alert('Auth is disabled.');
+    return;
+  }
+  if (!firebase.auth().currentUser) {
+    signIn();
+  } else {
+    signOut();
+  }
+}
+
+/**
+ * Sends the user's vote to the server.
+ * @param team
+ * @returns {Promise<void>}
+ */
+async function attendance() {
+  console.log(`Checking in the attendance`);
+  if (firebase.auth().currentUser || authDisabled()) {
+    console.log("Attendance function called with"); 
+
+    try {
+      const token = await createIdToken();
+
+    //fetch name from google people api
+      const res = await fetch('https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      
+      const profile = await res.json();
+      const name = profile.names?.[0]?.displayName;
+
+      const response = await fetch('/', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Authorization': `Bearer ${token}`
+          },
+          body: `name=${encodeURIComponent(name)}`
+      });
+
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      window.location.reload();
+      
+    } catch (err) {
+      console.log(`Error when submitting vote: ${err}`);
+      window.alert('Something went wrong... Please try again!');
+    }
+  } else {
+    window.alert('User not signed in.');
+  }
+}
